1fbd7b19687c767e1126c910df78df9840c9105e
fabianchs <fabichasola@hotmail.com>
2021-03-17 22:23:40 +0000

    avance final

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,30 +1,56 @@
-import React from "react";
-
+import React, { useState } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	let b1;
+	let b2;
+	let b3;
+
+	const [color, setColor] = useState("rojo");
+
+	if (color == "rojo") {
+		b1 = "btn btn-danger";
+		b2 = "btn btn-secondary";
+		b3 = "btn btn-secondary ";
+	} else if (color == "amarillo") {
+		b1 = "btn btn-secondary";
+		b2 = "btn btn-warning";
+		b3 = "btn btn-secondary";
+	} else if (color == "verde") {
+		b1 = "btn btn-secondary";
+		b2 = "btn btn-secondary";
+		b3 = "btn btn-success";
+	} else {
+		b1 = "btn btn-danger";
+		b2 = "btn btn-secondary";
+		b3 = "btn btn-secondary ";
+	}
+
 	return (
 		<div className="text-center mt-5 d-flex justify-content-center">
-			<div id="contenedor" className="bg-warning rounded-pill">
+			<div id="contenedor" className="bg-dark rounded-pill">
 				<div className="row d-flex justify-content-center" id="separar">
 					<button
 						type="button"
 						id="luz"
-						className="btn btn-danger"></button>
+						className={b1}
+						onClick={() => setColor("rojo")}></button>
 				</div>
 				<div className="row d-flex justify-content-center" id="separar">
 					<button
 						type="button"
 						id="luz"
-						className="btn btn-danger"></button>
+						className={b2}
+						onClick={() => setColor("amarillo")}></button>
 				</div>
 				<div className="row d-flex justify-content-center" id="separar">
 					<button
 						type="button"
 						id="luz"
-						className="btn btn-danger"></button>
+						className={b3}
+						onClick={() => setColor("verde")}></button>
 				</div>
 			</div>
 		</div>
